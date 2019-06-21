a3dfb3d554a2eeca5f7b813fa44a814433ec6f36
Zhang <yichen.zhang@anl.gov>
2019-06-21 12:50:24 -0500

    working

--- Hello_World/test.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(){
-  std::cout << "By today 2019 0611 Yichen Zhang enters a new world" ;
-  return 0;
-}
--- VS_Project_Test/HelloWorld/HelloWorld/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include <iostream> 
-using namespace std;
-
-int main()
-{
-	cout << "Hello World! ";
-	return 0;
-}
