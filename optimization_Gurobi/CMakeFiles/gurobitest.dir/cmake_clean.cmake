3852fd24f5e10f800b4f1f10186b4601beb75fc2
Yichen <31248391+whoiszyc@users.noreply.github.com>
2019-07-27 23:54:40 -0500

    test gurobi failed

---
+++ optimization_Gurobi/CMakeFiles/gurobitest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gurobitest.dir/exam_1.cpp.o"
+  "gurobitest.pdb"
+  "gurobitest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gurobitest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
